dfebd1e3564702447f9a5b0c216377535b5cab26
shreyder.ki <kr.shreyder@gmail.com>
2023-10-17 21:56:36 +0300

    Added 4 lab work

--- lab4/form.php
+++ lab4/form.php
@@ -1,23 +1,33 @@
 <?php require 'header.php' ?>
 
 <?php
-    if (isset($_GET['N'])) {$fio =$_GET['N'];} else {$fio='';}
-    if (isset($_GET['E'])) {$email =$_GET['E'];} else {$email='';}
-    if (isset($_GET['R'])) {$source =$_GET['R'];} else {$source='';}
+if (isset($_GET['N'])) {
+    $name = $_GET['N'];
+} else {
+    $name = '';
+}
+if (isset($_GET['E'])) {
+    $email = $_GET['E'];
+} else {
+    $email = '';
+}
+if (isset($_GET['R'])) {
+    $source = $_GET['R'];
+} else {
+    $source = '';
+}
 ?>
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $fio = $_POST['fio'];
+    $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    $topic = $_POST['topic'];
+    $category = $_POST['category'];
     $file = $_POST['file'];
-    $consent = isset($_POST['consent']) ? 'Да' : 'Нет';
+    $agreement = isset($_POST['agreement']) ? 'Да' : 'Нет';
     $source = $_POST['source'];
-    
 
-    // Перенаправление на страницу home.php для вывода ответа.
-    header("Location: home.php?fio=$fio&message=$message&source=$source&file=$file");
+    header("Location: home.php?fio=$name&message=$message&source=$source&file=$file");
     exit();
 }
 ?>
@@ -26,31 +36,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <div class="container">
         <h1>Форма обратной связи</h1>
         <form action="home.php" method="post">
-            <input type="hidden" name="user_id" value="6">
-
             <div class="form-group">
                 <label class="form-label" for="name">ФИО</label>
-                <input id="name" type="text" name="name" placeholder="Иванов Иван Иванович" required>
+                <input id="name" type="text" name="name" value="<?= $name ?>" placeholder=" Иванов Иван Иванович"
+                    required>
             </div>
 
-            <div class="form-group">
+            <div class=" form-group">
                 <label class="form-label" for="email">Email</label>
-                <input id="email" type="email" name="email" placeholder="Введите email">
+                <input id="email" type="email" name="email" value="<?= $email ?>" placeholder="example@example.com"
+                    required>
             </div>
 
             <div class="form-group">
                 <p class="form-label">Как вы узнали о нас?</p>
                 <input type="radio" name="source" id="advertising" value="advertising" checked>
                 <label for="advertising">Реклама в интернете</label>
-                <input type="radio" name="source" id="stands" value="friends">
-                <label for="friends">Реклама на стэндах</label><br>
                 <input type="radio" name="source" id="friends" value="friends">
                 <label for="friends">Рассказали знакомые</label>
             </div>
 
             <div class="form-group">
                 <label class="form-label" for="category">Категория обращения</label>
-                <select name="category" id="category">
+                <select name="category" id="category" required>
                     <option value="proposal">Предложение</option>
                     <option value="grievance">Жалоба</option>
                 </select>
@@ -62,12 +70,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             </div>
 
             <div class="form-group">
-                <label class="form-label" for="attachment">Вложение</label>
-                <input type="file" name="attachment" id="attachment">
+                <label class="form-label" for="file">Вложение</label>
+                <input type="file" name="file" id="file">
             </div>
 
             <div class="form-group">
-                <input type="checkbox" name="agreement" id="agreement" value="yes">
+                <input type="checkbox" name="agreement" id="agreement" value="no">
                 <label for="agreement">Даю согласие на обработку персональных данных</label>
             </div>
 
--- lab4/home.php
+++ lab4/home.php
@@ -1,29 +1,31 @@
 <?php
-include 'header.html';
+include 'header.php';
 ?>
 
 <body>
-    <?php
-    if (isset($_POST['fio'])) {
-        $fio = $_POST['fio'];
-        $file = $_POST['file'];
-        $message = $_POST['message'];
-        $topic = $_POST['topic'];
-        if (isset($_POST['source'])) {
-            $source = $_POST['source'];
-        } else {
-            $source = '';
+    <div class="container">
+        <?php
+        if (isset($_POST['name'])) {
+            $name = $_POST['name'];
+            $file = $_POST['file'];
+            $message = $_POST['message'];
+            $category = $_POST['category'];
+            if (isset($_POST['source'])) {
+                $source = $_POST['source'];
+            } else {
+                $source = '';
+            }
         }
-    }
 
-    echo "<h1>Ответ на ваше обращение:</h1>";
-    echo "<p><h2>ФИО:</h2> <span>$fio</span></p>";
-    echo "<p><h2>Текст обращения:</h2> <span>$message</span></p>";
-    echo "<p><h2>Источник:</h2> <span>$source</span></p></h2>";
-    echo "<p><h2>Вы прикрепили файл:</h2><span>$file</span></p></h2>";
+        echo "<h1>Ответ на ваше обращение:</h1>";
+        echo "<p><h2>ФИО:</h2> <span>$name</span></p>";
+        echo "<p><h2>Текст обращения:</h2> <span>$message</span></p>";
+        echo "<p><h2>Источник:</h2> <span>$source</span></p></h2>";
+        echo "<p><h2>Вы прикрепили файл:</h2><span>$file</span></p></h2>";
 
-    echo '<br><a class="button" href="index.php?N=' . $_POST['fio'] . '&E=' . $_POST['email'] . '&R='.$source. '">Заполнить снова</a></p></br>';
-    ?>
+        echo '<br><a class="button" href="index.php?N=' . $_POST['name'] . '&E=' . $_POST['email'] . '&R=' . $source . '">Заполнить снова</a></p></br>';
+        ?>
+    </div>
 </body>
 
-</html> 
+</html>
