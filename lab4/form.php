6306e932a4a2807ed72d8fdd2e07b877f402b5bf
shreyder.ki <kr.shreyder@gmail.com>
2023-10-18 11:42:35 +0300

    Added 4 lab work

--- lab4/form.php
+++ lab4/form.php
@@ -17,6 +17,7 @@ if (isset($_GET['R'])) {
     $source = '';
 }
 ?>
+
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $name = $_POST['name'];
@@ -36,54 +37,58 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <div class="container">
         <h1>Форма обратной связи</h1>
         <form action="home.php" method="post">
+            <input type="hidden" name="user_id" value="6">
+
             <div class="form-group">
-                <label class="form-label" for="name">ФИО</label>
+                <label class="form-label" for="name">ФИО:</label>
                 <input id="name" type="text" name="name" value="<?= $name ?>" placeholder=" Иванов Иван Иванович"
                     required>
             </div>
 
             <div class=" form-group">
-                <label class="form-label" for="email">Email</label>
+                <label class="form-label" for="email">Email:</label>
                 <input id="email" type="email" name="email" value="<?= $email ?>" placeholder="example@example.com"
                     required>
             </div>
 
             <div class="form-group">
                 <p class="form-label">Как вы узнали о нас?</p>
-                <input type="radio" name="source" id="advertising" value="advertising" checked>
-                <label for="advertising">Реклама в интернете</label>
-                <input type="radio" name="source" id="friends" value="friends">
-                <label for="friends">Рассказали знакомые</label>
-            </div>
+                <input type="radio" name="source" id="advertising" value="Реклама в интернете" <?php if ($source == 'Реклама из интернета')
+                    echo ' checked="checked"' ?> required>
+                    <label for="advertising">Реклама в интернете</label>
+                    <input type="radio" name="source" id="friends" value="Рассказали друзья" <?php if ($source == 'Рассказали друзья')
+                    echo ' checked="checked"' ?> required>
+                    <label for="friends">Рассказали знакомые</label>
+                </div>
 
-            <div class="form-group">
-                <label class="form-label" for="category">Категория обращения</label>
-                <select name="category" id="category" required>
-                    <option value="proposal">Предложение</option>
-                    <option value="grievance">Жалоба</option>
-                </select>
-            </div>
+                <div class="form-group">
+                    <label class="form-label" for="category">Тема обращения:</label>
+                    <select name="category" id="category" required>
+                        <option value="proposal">Предложение</option>
+                        <option value="grievance">Жалоба</option>
+                    </select>
+                </div>
 
-            <div class="form-group">
-                <label class="form-label" for="message">Текст сообщения</label>
-                <textarea name="message" id="message" cols="30" rows="10" placeholder="Введите сообщениe"> </textarea>
-            </div>
+                <div class="form-group">
+                    <label class="form-label" for="message">Текст сообщения:</label>
+                    <textarea name="message" id="message" cols="30" rows="10" placeholder="Введите сообщениe"> </textarea>
+                </div>
 
-            <div class="form-group">
-                <label class="form-label" for="file">Вложение</label>
-                <input type="file" name="file" id="file">
-            </div>
+                <div class="form-group">
+                    <label class="form-label" for="file">Вложение:</label>
+                    <input type="file" name="file" id="file">
+                </div>
 
-            <div class="form-group">
-                <input type="checkbox" name="agreement" id="agreement" value="no">
-                <label for="agreement">Даю согласие на обработку персональных данных</label>
-            </div>
+                <div class="form-group">
+                    <input type="checkbox" name="agreement" id="agreement" value="no">
+                    <label for="agreement">Даю согласие на обработку персональных данных</label>
+                </div>
 
-            <input class="btn" type="submit" value="Отправить">
+                <input class="btn" type="submit" value="Отправить">
 
-            <a href="index.html">Назад</a>
-        </form>
-    </div>
-</main>
+                <a href="index.php">Назад</a>
+            </form>
+        </div>
+    </main>
 
 <?php require 'footer.php' ?>
--- lab4/home.php
+++ lab4/home.php
@@ -18,12 +18,19 @@ include 'header.php';
         }
 
         echo "<h1>Ответ на ваше обращение:</h1>";
-        echo "<p><h2>ФИО:</h2> <span>$name</span></p>";
-        echo "<p><h2>Текст обращения:</h2> <span>$message</span></p>";
-        echo "<p><h2>Источник:</h2> <span>$source</span></p></h2>";
-        echo "<p><h2>Вы прикрепили файл:</h2><span>$file</span></p></h2>";
+        echo '<p> Здравствуйте, ' . $name . '</p>';
+        if ($category == 'propose') {
+            echo '<p>Спасибо за ваше предложение:</p>';
+            echo '<textarea>' . $message . '</textarea>';
+        } else {
+            echo '<p>Мы рассмотрим Вашу жалобу:</p>';
+            echo '<p>' . $message . '</p>';
+        }
 
-        echo '<br><a class="button" href="index.php?N=' . $_POST['name'] . '&E=' . $_POST['email'] . '&R=' . $source . '">Заполнить снова</a></p></br>';
+        echo "<p><h2>Источник:</h2>$source</p>";
+        echo "<p><h2>Вы прикрепили файл:</h2>$file</p>";
+
+        echo '<br><a class="button" href="form.php?N=' . $name . '&E=' . $_POST['email'] . '&R=' . $source . '">Заполнить снова</a></p></br>';
         ?>
     </div>
 </body>
